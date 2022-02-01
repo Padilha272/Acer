c76be160e256423a3631d5261529b6c0108b2db6
Matheus Padilha <matheuspadilha48@hotmail.com>
2022-02-01 11:13:37 -0700

    primeiro commit Acer

---
+++ Projetos Java/PrimeiroProjeto/src/App.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args)  {
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Olá mundo!");
+    }
+}
